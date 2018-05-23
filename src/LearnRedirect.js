0efafef15a3ca99321d03939937572ac2850366f
SreeraG <sreerag.nair@built.io>
2018-05-23 19:10:55 +0530

    Learning Redirect

---
+++ src/LearnRedirect.js
@@ -0,0 +1,55 @@
+
+
+import React, { Component } from 'react';
+import { Link, BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { Button } from 'antd';
+
+const t = () => {
+    return (
+        <h1>TARGET COMPONENT</h1>
+    )
+}
+
+const b = () =>{
+    return(
+        <Button onClick={this.callMe.bind(this)} > Click me! </Button>
+    )
+}
+
+class LearnRedirect extends Component {
+    state = {
+        x: false
+    };
+
+    callMe() {
+        this.setState({ x: true })
+    }
+
+    renderRedirect() {
+        if (this.state.x)
+            return <Redirect to='/target' />
+        // return <div><h1>CLICKED!!!</h1></div>
+    }
+
+
+    render() {
+
+        return (
+            <Router>
+                <div>
+                {this.renderRedirect()}
+                    <Switch>
+                        
+                        <Route exact path='/target' component={t} />
+                        <Route exact path='/' component={b} />
+                        
+                    </Switch>
+                    {/* <Link to = '/target'>{b}</Link> */}
+                    <Button onClick={this.callMe.bind(this)} > Click me! </Button>
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default LearnRedirect;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import LearnRedirect from './LearnRedirect';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    // <App />,
+    <LearnRedirect />,
+    document.getElementById('root'));
 registerServiceWorker();
